f4cd712cfcea2671f24df4dc91e6eeecfaf960a8
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2021-12-04 21:42:10 +0100

    Piano ready

---
+++ src/components/PianoKey/index.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const PianoKey = ({ path, id }) => {
+    const pianoSound = `/audio/${id}.mp3`;
+
+    const playSound = () => {
+        let s = new Audio(pianoSound);
+        s.play();
+    }
+
+    return (
+        <a href='#' onClick={playSound}>
+            {path}
+        </a>
+    )
+}
+
+export default PianoKey
